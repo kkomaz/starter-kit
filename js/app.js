a4fbfb4b81e461d1941a646137c76aac9aa32302
kkomaz <leeajl006@gmail.com>
2015-04-26 16:55:23 -0400

    dynamic resources via lesson 3

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.route('about');
   this.resource('products');
+  this.resource('product', { path: '/products/:title' });
+  this.resource('contacts');
+  this.resource('contact', { path: '/contacts/:name' });
 });
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
@@ -22,8 +25,23 @@ App.AboutController = Ember.Controller.extend({
 });
 
 App.ProductsRoute = Ember.Route.extend({
-  model: function(){
+  model: function() {
     return App.PRODUCTS;
+  }
+});
+App.ProductRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.PRODUCTS.findBy('title', params.title); 
+  }
+});
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return App.CONTACTS;
+  }
+});
+App.ContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.CONTACTS.findBy('name', params.name);
   }
 });
 
@@ -31,16 +49,28 @@ App.PRODUCTS = [
   {
     title: 'Flint',
     price: 99,
-    description: 'Flint is...',
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     isOnSale: true,
     image: 'images/products/flint.png'
   },
   {
     title: 'Kindling',
-    price: 49,
-    description: 'Kindling is...',
+    price: 249,
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
     isOnSale: false,
-    image: 'images/products/kindling.png'    
+    image: 'images/products/kindling.png'
   }
 ];
 
+App.CONTACTS = [
+  {
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'images/contacts/giamia.png'
+  },
+  {
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'images/contacts/anostagia.png'
+  }
+];
