7c33964aa58dc0e37874984d7c8d342332f0c7a5
kkomaz <leeajl006@gmail.com>
2015-04-26 12:38:47 -0400

    Lesson 3: Resource routes

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.route('about');
+  this.resource('products');
 });
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
@@ -12,11 +13,34 @@ App.IndexController = Ember.Controller.extend({
     return (new Date()).toDateString();
   }.property()
 });
-
 App.AboutController = Ember.Controller.extend({
-  contactName: "Alex",
+  contactName: 'Anostagia',
   avatar: 'images/avatar.png',
-  open: function(){
+  open: function() {
     return ((new Date()).getDay() === 0) ? "Closed" : "Open";
   }.property()
 });
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function(){
+    return App.PRODUCTS;
+  }
+});
+
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is...',
+    isOnSale: true,
+    image: 'images/products/flint.png'
+  },
+  {
+    title: 'Kindling',
+    price: 49,
+    description: 'Kindling is...',
+    isOnSale: false,
+    image: 'images/products/kindling.png'    
+  }
+];
+
