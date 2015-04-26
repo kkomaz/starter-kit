acef316e081a9381e1fa583e3180f52189c19ec9
kkomaz <leeajl006@gmail.com>
2015-04-26 12:10:30 -0400

    lesson 2 complete

--- js/app.js
+++ js/app.js
@@ -5,10 +5,18 @@ App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.route('about');
 });
-
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
   logo: 'images/logo-small.png',
-  time: function(){
-    return (new Date()).toDateString();}.property()
+  time: function() {
+    return (new Date()).toDateString();
+  }.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+  contactName: "Alex",
+  avatar: 'images/avatar.png',
+  open: function(){
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
+  }.property()
 });
